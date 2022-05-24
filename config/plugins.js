bc3f543fa6a72687fdf1699f8170bf83f8bf38c5
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-05-24 13:51:42 +0000

    Refactor GCS bucket name

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     config: {
       provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
       providerOptions: {
-        bucketName: env("GOOGLE_CLOUD_STORAGE_BUCKET_NAME"),
+        bucketName: env("UPLOAD_GCS_BUCKET_NAME"),
         publicFiles: false,
         uniform: false,
         basePath: "",
